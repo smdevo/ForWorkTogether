d5bd9b382eb682914bed7db0894c640363569a43
Samandar <samandartoshpulatov0704@gmail.com>
2025-01-14 19:27:16 +0500

    Initial Commit

---
+++ ForWorkTogether/ForWorkTogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForWorkTogetherApp.swift
+//  ForWorkTogether
+//
+//  Created by Samandar on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ForWorkTogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
